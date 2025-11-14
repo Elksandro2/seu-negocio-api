1ec126bf18d1b1e68384090fa2c666919558d9bb
Elksandro2 <silvaelksandro@gmail.com>
2025-11-14 17:19:09 -0300

    feat: adicionando mais rotas

--- src/main/java/com/elksandro/seunegocio/config/SecurityConfig.java
+++ src/main/java/com/elksandro/seunegocio/config/SecurityConfig.java
@@ -37,6 +37,9 @@ public class SecurityConfig {
                     request.requestMatchers("/", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll();
                     request.requestMatchers(HttpMethod.POST, "/v1/user/register").permitAll();
                     request.requestMatchers(HttpMethod.POST, "/v1/user/login").permitAll();
+                    request.requestMatchers(HttpMethod.GET, "/v1/user/me").authenticated();
+                    request.requestMatchers(HttpMethod.PATCH, "/v1/user/**").authenticated();
+                    request.requestMatchers(HttpMethod.DELETE, "/v1/user/**").authenticated();
                     request.requestMatchers(HttpMethod.GET, "/v1/businesses/**").permitAll();
                     request.requestMatchers(HttpMethod.POST, "/v1/businesses").authenticated();
                     request.requestMatchers(HttpMethod.PATCH, "/v1/businesses/**").authenticated();
